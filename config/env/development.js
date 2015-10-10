848b89c9b81977444c7e5c84af82557d6fdaaf65
joshfermin <joshfermin@gmail.com>
2015-10-10 11:09:58 -0600

    adding mongolabs db

--- config/env/development.js
+++ config/env/development.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  db: 'mongodb://' + (process.env.DB_PORT_27017_TCP_ADDR || 'localhost') + '/mean-dev',
+  db: 'mongodb://' + ('hackcu:hackcu@ds041643.mongolab.com:41643/divemusic' || 'localhost') + '/mean-dev',
   debug: true,
   logging: {
     format: 'tiny'
