40565c449e05451fff0638ce6bf359c209ed2425
joshfermin <joshfermin@gmail.com>
2015-10-11 14:42:25 -0600

    started working on frontend

--- frontend/client/src/application.js
+++ frontend/client/src/application.js
@@ -1,21 +1,43 @@
 var app = angular.module('divemusic', []);
 
 app.controller('HomeController', ['$http', function($http) {
-  var map = new Datamap({element: document.getElementById('container')});
-  
-  var ui = this;
-  ui.response = false;
+	var map = new Datamap({
+		element: document.getElementById('container'),
+		fills: {
+				HIGH: '#afafaf',
+				LOW: '#123456',
+				MEDIUM: 'blue',
+				UNKNOWN: 'rgb(0,0,0)',
+				defaultFill: '#afafaf'
+		},
+		done: function(datamap) {
+			datamap.svg.selectAll('.datamaps-subunit').on('click', function(geography) {
+					var m = {};
+					m[geography.id] = '#000000';
+					datamap.updateChoropleth(m);
+					var country = geography.properties.name;
+					$http({
+						method: 'GET',
+						url: '/country/' + country
+					})
 
-  // ui.getSummoner = function() {
-  //   if (ui.summonerName) {
-  //     var querystring = (ui.summonerName).toLowerCase().replace(/ /g, '');
-  //     $http.get('/summoner/'+querystring).success(function(data) {
-  //       ui.resData = data;
-  //       ui.timeData = timeParse(data);
-  //       ui.summoner = data[0].summoner_id;
-  //       drawCal();
-  //       ui.response = true;
-  //     });
-  //   }
-  // };
+				});
+			}
+	});
+	
+	var ui = this;
+	ui.response = false;
+
+	ui.getSummoner = function() {
+		if (ui.summonerName) {
+			var querystring = (ui.summonerName).toLowerCase().replace(/ /g, '');
+			$http.get('/summoner/'+querystring).success(function(data) {
+				ui.resData = data;
+				ui.timeData = timeParse(data);
+				ui.summoner = data[0].summoner_id;
+				drawCal();
+				ui.response = true;
+			});
+		}
+	};
 }]);
--- frontend/server/routes.js
+++ frontend/server/routes.js
@@ -1,6 +1,6 @@
 var db     = require('./mongo.js'),
 // timeParser = require('./timeParse.js');
-games      = db.dataInit('track');
+tracks      = db.dataInit('track');
 
 
 module.exports = function(app){
@@ -12,16 +12,8 @@ module.exports = function(app){
   // ***
   // Gets track for a country
   // ***
-  app.get('/summoner/:summoner_name', function(req,res){
-  	summoners.find({ summoner_short:req.params.summoner_name }, function(err,doc){
-  		if(err) res.send(err);
-      doc = JSON.stringify(doc);
-      doc = JSON.parse(doc);
-      games.find({ summoner_id:doc[0].summoner_id }, function(err,games){
-        cachedGames = games;
-        if(err) res.send(err);
-        res.send(games);
-      });
-  	});
+  app.get('/toptracks/:country', function(req,res){
+  	var q = tracks.find({country:req.params.country});
+    console.log(q);
   });
 };
